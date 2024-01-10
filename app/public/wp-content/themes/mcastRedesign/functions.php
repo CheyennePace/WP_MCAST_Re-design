83ace03dae6f672d97c1ee8e88cf4a7e8ba6dcf8
CheyennePace <56068119+CheyennePace@users.noreply.github.com>
2024-01-11 00:19:51 +0100

    fixed library page... continued footer

--- app/public/wp-content/themes/mcastRedesign/functions.php
+++ app/public/wp-content/themes/mcastRedesign/functions.php
@@ -1,6 +1,6 @@
 <?php
 
-show_admin_bar(true);
+show_admin_bar(false);
 
 require_once("lib/enqueue-assets.php");
 require_once("lib/navigation.php");
@@ -22,13 +22,7 @@ add_filter('excerpt_length', 'firstexample_excerptlength');
 function firstexample_themefooter(){
     $footer_bg = get_theme_mod('custom_theme_footer_bg', '#FFFFFF');
     $footer_text = get_theme_mod('custom_theme_txt', '#000000');
-    echo "<div class='container-fluid' style='background-color:{$footer_bg};'>
-            <div class='row'>
-                <div class='col text-center' style='color: {$footer_text}'>
-                    <p>© Copyright MCAST 2023</p>
-                </div>
-            </div>
-          </div>";
+    
 }
 
 add_action('get_footer','firstexample_themefooter');
--- app/public/wp-content/themes/mcastRedesign/page-library.php
+++ app/public/wp-content/themes/mcastRedesign/page-library.php
@@ -8,35 +8,7 @@
     <img src="<?php  echo $banner_img_lib;?>"" class="img-fluid" alt="Responsive image">
 </div>
 
-<!--custom search-->
-<div id="book-search">
-    <form action="<?php echo esc_url(home_url('/library')); ?>" id="searchform" method="get">
-        <div style = "display: inline-flex; ">
-            <label for="search" class="screen-reader-text">Search for:</label>
-            <input type="text" id="search" name="search" value="" />
-            <input type="hidden" name="post_type" value="book" /> 
-            <input type="submit" value="Search" id="searchsubmit" />
-        </div>
-    </form>
 
-    <?php
-    add_action('parse_query', function($query){
-
-        // not the search request
-        if(!$query->is_search)
-          return;
-      
-        // validate post type here (you should provide a white-list)
-        $post_type = isset($_GET['post_type']) ? sanitize_key($_GET['post_type']) : false;
-      
-        // adjust the query
-        if($post_type && post_type_exists($post_type))
-          $query->set('post_type', $post_type);
-      
-      });
-    
-?>
-</div>
 
 
 <div class="container">
