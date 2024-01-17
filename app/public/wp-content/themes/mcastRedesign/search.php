6d784b18a5d56e4ce1185ae75bbc65cad8cd3bb3
CheyennePace <56068119+CheyennePace@users.noreply.github.com>
2024-01-17 09:59:09 +0100

    updated Styling issues
    fixed some more styling issues
    fixed hover on navbar

--- app/public/wp-content/themes/mcastRedesign/search.php
+++ app/public/wp-content/themes/mcastRedesign/search.php
@@ -3,14 +3,13 @@
 <!-- The Loop -->
 <div class="container">
 	<div class="row">
-<?php 
-	$args = array('section_title' => 'Search Results');
-	get_template_part("template-parts/loop", null, $args);
-?>
-<div class="col-4">
-			<!-- Prepped for Sidebar -->
-           <?php get_sidebar(); ?>
-		</div>
+<?php $args = array('section_title' => 'Search Results'); ?>
+	<div class="contnt-in-search" style="padding-bottom: 5%;">
+	<?php get_template_part("template-parts/loop-search", null, $args);
+	?>
+	</div>
+	
+
 	</div>
 </div>
 <?php get_footer(); ?>
